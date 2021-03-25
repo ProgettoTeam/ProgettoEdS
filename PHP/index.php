7fe3173e1ffb1e9cf3ab08d54cdd0fb6bc996e4d
teomirza02 <toader.mirza.studenti@isii.it>
2021-03-25 08:29:44 +0100

    Update index.php

--- PHP/index.php
+++ PHP/index.php
@@ -23,6 +23,10 @@
     
   
     <!-- ###################### -->
+    <!-- jQuery -->
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script> 
+<!-- jQuery UI -->
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
     
     
     <title>HomePage</title>
@@ -110,11 +114,105 @@
 </div>
 
 <h2 class="titolo">Viualizza qui i Parchi:</h2>
+
 <div id="map"></div>
   <script
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVmxa27-uGFIPD7pW_P5l0mxsXglRxpos&callback=initMap&libraries=&v=weekly"
-    async
-  ></script>
+    async>
+  </script>
+
+
+<table id="tableData" class="table table-bordered table-striped">
+   <thead>
+      <tr>
+         <th>id</th>
+         <th>first name</th>
+         <th>surname</th>
+         <th>number</th>
+      </tr>
+   </thead>
+   <tbody>
+      <tr>
+         <td>1</td>
+         <td>Frank</td>
+         <td>Shoulder</td>
+         <td>1246</td>
+      </tr>
+      <tr>
+         <td>2</td>
+         <td>John</td>
+         <td>Jameson</td>
+         <td>4564</td>
+      </tr>
+      <tr>
+         <td>3</td>
+         <td>Philip</td>
+         <td>Jenkins</td>
+         <td>4456</td>
+      </tr>
+      <tr>
+         <td>4</td>
+         <td>Maria</td>
+         <td>Carlston</td>
+         <td>4456</td>
+      </tr>
+      <tr>
+         <td>5</td>
+         <td>Julia</td>
+         <td>Tampelton</td>
+         <td>1246</td>
+      </tr>
+      <tr>
+         <td>6</td>
+         <td>Jane</td>
+         <td>Conor</td>
+         <td>4456</td>
+      </tr>
+      <tr>
+         <td>7</td>
+         <td>Susan</td>
+         <td>Crane</td>
+         <td>1246</td>
+      </tr>
+      <tr>
+         <td>8</td>
+         <td>Lucas</td>
+         <td>Fenric</td>
+         <td>4456</td>
+      </tr>
+      <tr>
+         <td>8</td>
+         <td>Mark</td>
+         <td>Fenric</td>
+         <td>4456</td>
+      </tr>
+      <tr>
+         <td>9</td>
+         <td>Hilde</td>
+         <td>Mayer</td>
+         <td>4456</td>
+      </tr>
+      <tr>
+         <td>10</td>
+         <td>John</td>
+         <td>Tron</td>
+         <td>1246</td>
+      </tr>
+      <tr>
+         <td>11</td>
+         <td>Hans</td>
+         <td>Stark</td>
+         <td>4564</td>
+      </tr>
+   </tbody>
+</table>
+
+<script type="text/javascript" src="../JS/index.js"></script> 
+<script type="text/javascript">
+            $(document).ready(function() {
+                $('#tableData').paging({limit:5});
+            });
+</script>
 
   <?php include 'footer.php'?>
 
