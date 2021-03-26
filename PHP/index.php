a4e284f0b39d4e79a729cb2355a710378714312b
teomirza02 <toader.mirza.studenti@isii.it>
2021-03-26 18:47:56 +0100

    Update index.php

--- PHP/index.php
+++ PHP/index.php
@@ -8,26 +8,7 @@
     
     <!-- librerie importate -->
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
-    
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-
-    <link rel="stylesheet" href="../CSS/style.css">
-    <script src="../JS/index.js"></script>
-
-    
-  
-    <!-- ###################### -->
-    <!-- jQuery -->
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script> 
-<!-- jQuery UI -->
-<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
-    
+    <?php  include("librerie.php");?>
     
     <title>HomePage</title>
 </head>
