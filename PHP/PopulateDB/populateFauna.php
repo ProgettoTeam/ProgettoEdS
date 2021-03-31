98d8df7cccbd8206855a505ca89d6b44430da296
teomirza02 <toader.mirza.studenti@isii.it>
2021-03-31 15:53:51 +0200

    Create populateFauna.php

---
+++ PHP/PopulateDB/populateFauna.php
@@ -0,0 +1,12 @@
+<?php
+$conn = mysqli_connect("localhost", "parco", "", "parco");
+$fauna = fopen("../../CSV/Fauna.csv", "r");
+
+while(($column = fgetcsv($fauna, 500, ",")) !== false)
+{
+    $InsertFauna = "insert into fauna(IdFauna,IsAdult,Sesso,Salute,path_immagine,fk_IdCategoria,fk_IdParco) values('$column[0]','$column[1]', '$column[2]', '$column[3]', '$column[4]', '$column[5]', '$column[6]')";
+    $result = mysqli_query($conn, $InsertFauna);
+}
+fclose($fauna);
+mysqli_close($conn);
+?>
