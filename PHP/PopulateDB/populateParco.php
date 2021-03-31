5b1ee028ce3e049a21d194b3579befbd324a74b5
teomirza02 <toader.mirza.studenti@isii.it>
2021-03-31 15:53:55 +0200

    Create populateParco.php

---
+++ PHP/PopulateDB/populateParco.php
@@ -0,0 +1,12 @@
+<?php
+$conn = mysqli_connect("localhost", "parco", "", "parco");
+$parchi = fopen("../../CSV/Parchi.csv", "r");
+
+while(($column = fgetcsv($parchi, 220, ",")) !== false)
+{
+    $InsertParco = "insert into parco(IdParco,Nome,Luogo,Latitudine,Longitudine,Descrizione,path_immagine,fk_IdAmministratore) values('$column[0]','$column[1]', '$column[2]', '$column[3]', '$column[4]', '$column[5]', '$column[6]', '$column[7]')";
+    $result = mysqli_query($conn, $InsertParco);
+}
+fclose($parchi);
+mysqli_close($conn);
+?>
