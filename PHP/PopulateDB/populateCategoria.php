402064e9b5d7a136f59ff919cbeffed19ff3efc6
teomirza02 <toader.mirza.studenti@isii.it>
2021-04-01 19:30:16 +0200

    Update populateCategoria.php

--- PHP/PopulateDB/populateCategoria.php
+++ PHP/PopulateDB/populateCategoria.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "parco", "", "parco");
+include '../DBconnection.php';
 $categorie = fopen("../../CSV/Categorie_fauna.csv", "r");
 
 while(($column = fgetcsv($categorie, 30, ",")) !== false)
@@ -8,5 +8,5 @@ while(($column = fgetcsv($categorie, 30, ",")) !== false)
     $result = mysqli_query($conn, $InsertCategoria);
 }
 fclose($categorie);
-mysqli_close($conn);
+include '../DBclose.php';
 ?>
