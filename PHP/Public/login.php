705cb3bfa4d61542b8d97a23d5c06bd3acab575d
teomirza02 <toader.mirza.studenti@isii.it>
2021-04-02 09:25:44 +0200

    Update login.php

--- PHP/Public/login.php
+++ PHP/Public/login.php
@@ -1,26 +1,33 @@
 <!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Login</title>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../CSS/style.css">
 
-        <?php include ("../librerie.php")?>
-        </head>
-	<body style="background-color: #1d814a;-webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;box-sizing: border-box;font-family:Roboto;"> 
-		<div class="login">
-			<img src="../../CSS/logo-flora-fauna.png" class="logologin">
-			<form action="authenticate.php" method="post">
-				<label for="username">
-					<i class="fas fa-user"></i>
-				</label>
-				<input type="text" name="username" placeholder="Username" id="username" required>
-				<label for="password">
-					<i class="fas fa-lock"></i>
-				</label>
-				<input type="password" name="password" placeholder="Password" id="password" required>
-				<input type="submit" value="Login">
-			</form>
-		</div>
-	</body>
+    <?php include("../librerie.php");
+    ?>
+
+</head>
+<body style="background-color: #1d814a;">
+
+    <div class="login">
+        <img src="../../CSS/logo-flora-fauna.png" class="logologin">
+
+        <form action="authenticate.php" method="post">
+            <label for="username" style="margin-top: 3px;">
+                <i class="fas fa-user"></i>
+            </label>
+            <input type="text" name="username" placeholder="Username" id="username" required>
+            <label for="password">
+                <i class="fas fa-lock"></i>
+            </label>
+            <input type="password" name="password" placeholder="Password" id="password" required>
+            <input type="submit" value="Login">
+        </form>
+    </div>
+
+</body>
 </html>
