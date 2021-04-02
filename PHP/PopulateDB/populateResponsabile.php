f313826fc0c55f48de51f1b2d3051dc629ecb70a
teomirza02 <toader.mirza.studenti@isii.it>
2021-04-02 09:25:42 +0200

    Update populateResponsabile.php

--- PHP/PopulateDB/populateResponsabile.php
+++ PHP/PopulateDB/populateResponsabile.php
@@ -4,24 +4,9 @@ $responsabili = fopen("../../CSV/Responsabili.csv", "r");
 
 while(($column = fgetcsv($responsabili, 300, ",")) !== false)
 {
-    $password = md5($column[4]);
-    echo $column[0];
-    echo '////////';
-    echo $column[1];
-    echo '////////';
-    echo $column[2];
-    echo '////////';
-    echo $column[3];
-    echo '////////';
-    echo $password;
-    echo '////////';
-    echo $column[5];
-    echo '////////';
-    echo $column[6];
-    echo '///////////////////////////////';
-    $InsertResponsabile = "insert into responsabile(IdResponsabile,Nome,Cognome,Email,Password,fk_IdAmministratore, fk_IdParco) values('$column[0]','$column[1]', '$column[2]', '$column[3]', '$password', '$column[5]', '$column[6]')";
+    $InsertResponsabile = "insert into responsabile(IdResponsabile,Nome,Cognome,Email,Password,fk_IdAmministratore, fk_IdParco) values('$column[0]','$column[1]', '$column[2]', '$column[3]', '$column[4]', '$column[5]', '$column[6]')";
     $result = mysqli_query($conn, $InsertResponsabile);
 }
 fclose($responsabili);
-include '../DBclose.php';
+mysqli_close($conn);
 ?>
