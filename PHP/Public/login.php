187db7bdda7f84e2b51bd5eb7cb45b5ce3fe0237
teomirza02 <toader.mirza.studenti@isii.it>
2021-04-04 14:16:06 +0200

    Update login.php

--- PHP/Public/login.php
+++ PHP/Public/login.php
@@ -1,10 +1,11 @@
+<?php include('../DAL.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
     <link rel="stylesheet" href="../../CSS/style.css">
 
     <?php include("../librerie.php");
@@ -16,16 +17,17 @@
     <div class="login">
         <img src="../../CSS/logo-flora-fauna.png" class="logologin">
 
-        <form action="authenticate.php" method="post">
+        <form action="login.php" method="POST">
+            <?php include('../Errors.php'); ?>
             <label for="username" style="margin-top: 3px;">
                 <i class="fas fa-user"></i>
             </label>
-            <input type="text" name="username" placeholder="Username" id="username" required>
+            <input type="text" name="email" placeholder="Email" id="username" required>
             <label for="password">
                 <i class="fas fa-lock"></i>
             </label>
             <input type="password" name="password" placeholder="Password" id="password" required>
-            <input type="submit" value="Login">
+            <input type="submit" name="login" value="Login">
         </form>
     </div>
 
