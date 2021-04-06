97dd5f6d5e6b42dbc9a9cadac2dadaf3a439d6ea
teomirza02 <toader.mirza.studenti@isii.it>
2021-04-06 15:04:36 +0200

    Update form-aggiungi.php

--- PHP/Admin/form-aggiungi.php
+++ PHP/Admin/form-aggiungi.php
@@ -9,12 +9,21 @@
     <title>form-aggiungi</title>
 </head>
 <body>
-<?php include ("../navbar.php")?>
+<?php 
+include ("../navbar.php");
+include '../DBconnection.php';
+$tabella = $_GET["tabella"];
+?>
+
+<h1 style="padding-left: 45%; padding-top: 11px">Aggiungi <?php echo $tabella ?></h1>
 
 <div class="container-form-modifica">
-  <div style="float:right;"> 
-    <label for="cars">Quanti elementi vuoi aggiungere?</label>
-    <select name="cars" id="cars" class="combobox">
+
+    
+  <!-- COMBOBOX -->
+  <div style="float:right; padding-top: 80px; margin-right:100px;"> 
+    <label for="cars">Righe</label>
+    <select name="cars" id="cars" class="combobox" onclick="Combobox(value)">
       <option value="1">1</option>
       <option value="2">2</option>
       <option value="3">3</option>
@@ -22,33 +31,62 @@
       <option value="5">5</option>
     </select>
   </div>
-  <form action="/pagina_che_gestisce.php">
-  <table id="table" class="table table-bordered table-striped">
-    <thead>
+  <!--------------------------------->
+  
+  <!-- AGGIUNTA RIGHE -->
+  <form action="login.php" method="POST">
+    <table id="table" class="table table-bordered table-striped">
+      <thead>
         <tr>
-          <th>Campo1</th>
-          <th>Campo2</th>
-          <th>Campo3</th>
-          <th>Campo4</th>
+          <?php
+            $query = "SELECT * from $tabella";
+            $result = mysqli_query($conn, $query);
+            // Get field information for all fields
+            $fieldinfo = mysqli_fetch_fields($result);
+            $cont = 0;
+            foreach ($fieldinfo as $val) {
+              if($cont > 1) {
+                $col = $val->name;
+                ?> 
+                  <th> <?php echo $col ?></th>
+                <?php
+              } else if($cont == 1) {
+                ?>
+                  <th> Immagine </th>
+                <?php
+              }
+              $cont++;
+            }
+            mysqli_free_result($result);
+          ?>
         </tr>
-    </thead>
-    <tbody>
-        <td>
-          <div class="container-img-tab" id="containerimghome">
-            <a href='PHP/Public/parco.php?IdParco=<?php echo $row['IdParco'] ?>'>
-              <img src="../../CSS/img_fauna/fenicottero.jpg" id="img_tabella"></img>
-              <div class="overlay-tab">
-                <div class="text">Aggiungi immagine</div>
-              </div>
-            </a>
-          </div>
-        </td>
-        <td style="margin-top:45%"><input type="text" id="campoN" name="CampoN-name" placeholder="Introduci campo2"></td>
-        <td><input type="text" id="campoN" name="CampoN-name" placeholder="Introduci campo3"></td>
-        <td><input type="text" id="campoN" name="CampoN-name" placeholder="Introduci campo4"></td>
-    </tbody>
-
-  </table>
+      </thead>
+      <tbody>
+          <?php
+            $cont = 0;
+            foreach ($fieldinfo as $val) {
+              if($cont > 1) {
+                $col = $val->name;
+                ?> 
+                  <td><input type="text" class="campoN" name="CampoN-name" placeholder="Introduci campo <?php echo $cont ?>"></td>
+                <?php
+              } else if($cont == 1) {
+                ?>    
+                  <td>
+                    <div class="container-img-tab" id="containerimghome">
+                      <img src="../../CSS/bianco.jpg" id="img_tabella"></img>
+                      <div class="overlay-tab">
+                        <div class="text">Inserisci immagine</div>
+                      </div>
+                    </div>
+                  </td>
+                <?php
+              }
+              $cont++;
+            }
+          ?>
+      </tbody>
+    </table>
 
 
     <!-- se vuoi creare altri campi copia questi elementi 
@@ -58,24 +96,29 @@
     -->
 
     <div class="rowimg">
-   <div class="columnbtn">
-        <input type="submit" value="Annulla" name="Annulla">
-   </div>
+      <div class="columnbtn">
+            <input type="submit" value="Annulla" name="Annulla">
+      </div>
 
-   <div class="columnbtn">
-      <input type="submit" value="Aggiungi" name="Aggiungi">
-   </div>
+      <div class="columnbtn">
+          <input type="submit" value="Aggiungi" name="Aggiungi">
+      </div>
 
-   <!-- puoi fare la verifica nella pagina 'pagina-che-gestisce.php' la verifica 
-    sul submit vedendo il testo che ha schiacciato
-    (verifica sul 'name' --annulla o modifica-- ) -->
-</div>
+      <!-- puoi fare la verifica nella pagina 'pagina-che-gestisce.php' la verifica 
+        sul submit vedendo il testo che ha schiacciato
+        (verifica sul 'name' --annulla o modifica-- ) -->
+    </div>
 
     
   </form>
 </div>
 
-<?php include ("../footer.php")?>
+<script src="../../JS/index.js"></script>
+
+<?php 
+include '../DBclose.php';
+include ("../footer.php");
+?>
 
 </body>
 </html>
