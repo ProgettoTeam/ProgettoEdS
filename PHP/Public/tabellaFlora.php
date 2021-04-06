d0de13f04dc0b4aeacc0d248bb39a26457a6101f
teomirza02 <toader.mirza.studenti@isii.it>
2021-04-06 15:04:50 +0200

    Update tabellaFlora.php

--- PHP/Public/tabellaFlora.php
+++ PHP/Public/tabellaFlora.php
@@ -6,20 +6,21 @@
 
 
 <div class="rowimg">
-   <div class="columnbtn">
+   <div class="column">
       <h2 class="titolo2" style="float:left;">Alberi</h2>
    </div>
-
-   <div class="columnbtn">
+   <div class="column">
       <div class="bottone">
-            <div class="search-container">
-                <form>
-                    <input type="text" placeholder="Search.." name="search">
-                    <button type="submit"><i class="fa fa-search"></i></button>
-                </form>
-        </div>
+         <a href="form-aggiungi.php?tabella=parco" class="btn btn-success btn-lg">
+            <span class="glyphicon glyphicon-plus-sign"></span> Aggiungi 
+         </a>
       </div>
    </div>
+   
+   <?php 
+      $tabella = 'alberi';
+      include 'Searchbar.php';
+   ?>
 </div>
 
 <table id="tableData2" class="table table-bordered table-striped">
@@ -34,20 +35,25 @@
    <tbody>
    <?php
       $query_albero = "SELECT * FROM flora WHERE Categoria = 'Albero' AND fk_IdParco = '$parco[0]'";
-      $result_albero = mysqli_query($conn, $query_albero);
-      while($albero = mysqli_fetch_array($result_albero))
-      { 
-         ?>
-         <tr>
-            <td>
-               <img src=" ../../<?php echo $albero['path_immagine_albero'] ?>" id="img_tabella"></img>
-            </td>
-            <td><?php echo $albero['GenereAlbero']?></td>
-            <td><?php echo $albero['TipoFoglie']?></td> 
-            <td><?php echo $albero['Stagione_fioritura'] ?></td>
-         </tr>
-         <?php
-      } 
+      if(isset($_POST["submit_alberi"])){
+         $genereAlbero = mysqli_real_escape_string($conn, $_POST['search']);
+         $query_albero = "SELECT * FROM flora WHERE GenereAlbero LIKE '$genereAlbero' AND flora.fk_IdParco = '$parco[0]'";
+      }
+      if($result_albero = mysqli_query($conn, $query_albero)) {
+         while($albero = mysqli_fetch_array($result_albero))
+         { 
+            ?>
+            <tr>
+               <td>
+                  <img src=" ../../<?php echo $albero['path_immagine_albero'] ?>" id="img_tabella"></img>
+               </td>
+               <td><?php echo $albero['GenereAlbero']?></td>
+               <td><?php echo $albero['TipoFoglie']?></td> 
+               <td><?php echo $albero['Stagione_fioritura'] ?></td>
+            </tr>
+            <?php
+         } 
+      }
       ?>
    </tbody>
 </table>
@@ -60,20 +66,22 @@
 <!--------------->
 
 <div class="rowimg">
-   <div class="columnbtn">
+   <div class="column">
       <h2 class="titolo2" style="float:left;">Arbusti</h2>
    </div>
 
-   <div class="columnbtn">
+   <div class="column">
       <div class="bottone">
-            <div class="search-container">
-                <form>
-                    <input type="text" placeholder="Search.." name="search">
-                    <button type="submit"><i class="fa fa-search"></i></button>
-                </form>
-        </div>
+         <a href="form-aggiungi.php?tabella=parco" class="btn btn-success btn-lg">
+            <span class="glyphicon glyphicon-plus-sign"></span> Aggiungi 
+         </a>
       </div>
    </div>
+   
+   <?php 
+      $tabella = 'arbusti';
+      include 'Searchbar.php' 
+   ?>
 </div>
 
 <table id="tableData3" class="table table-bordered table-striped">
@@ -88,6 +96,10 @@
    <tbody>
    <?php
       $query_arbusto = "SELECT * FROM flora WHERE Categoria = 'Arbusto' AND fk_IdParco = '$parco[0]'";
+      if(isset($_POST["submit_arbusti"])){
+         $specieArbusto = mysqli_real_escape_string($conn, $_POST['search']);
+         $query_arbusto = "SELECT * FROM flora WHERE SpecieArbusto LIKE '$specieArbusto' AND flora.fk_IdParco = '$parco[0]'";
+      }
       $result_arbusto = mysqli_query($conn, $query_arbusto);
       while($arbusto = mysqli_fetch_array($result_arbusto))
       { 
@@ -115,20 +127,21 @@
 
 
 <div class="rowimg">
-   <div class="columnbtn">
+   <div class="column">
       <h2 class="titolo2" style="float:left;">Piante erbacee</h2>
    </div>
-
-   <div class="columnbtn">
+   <div class="column">
       <div class="bottone">
-            <div class="search-container">
-                <form>
-                    <input type="text" placeholder="Search.." name="search">
-                    <button type="submit"><i class="fa fa-search"></i></button>
-                </form>
-        </div>
+         <a href="form-aggiungi.php?tabella=parco" class="btn btn-success btn-lg">
+            <span class="glyphicon glyphicon-plus-sign"></span> Aggiungi 
+         </a>
       </div>
    </div>
+   
+   <?php 
+      $tabella = 'piante erbacee';
+      include 'Searchbar.php';
+   ?>
 </div>
 
 <table id="tableData4" class="table table-bordered table-striped">
@@ -142,18 +155,22 @@
    </thead>
    <tbody>
    <?php
-      $query_arbusto = "SELECT * FROM flora WHERE Categoria = 'Pianta erbacea' AND fk_IdParco = '$parco[0]'";
-      $result_arbusto = mysqli_query($conn, $query_arbusto);
-      while($arbusto = mysqli_fetch_array($result_arbusto))
+      $query_piantaErbacea = "SELECT * FROM flora WHERE Categoria = 'Pianta erbacea' AND fk_IdParco = '$parco[0]'";
+      if(isset($_POST["submit_pianteErbacee"])){
+         $ClassificazionePianteErbacee = mysqli_real_escape_string($conn, $_POST['search']);
+         $query_piantaErbacea = "SELECT * FROM flora WHERE ClassificazionePianteErbacee LIKE '$ClassificazionePianteErbacee' AND flora.fk_IdParco = '$parco[0]'";
+      }
+      $result_piantaErbacea = mysqli_query($conn, $query_piantaErbacea);
+      while($piantaErbacea = mysqli_fetch_array($result_piantaErbacea))
       { 
          ?>
          <tr>
             <td>
-               <img src=" ../../<?php echo $arbusto['path_immagine_PiantaErbacea'] ?>" id="img_tabella"></img>
+               <img src=" ../../<?php echo $piantaErbacea['path_immagine_PiantaErbacea'] ?>" id="img_tabella"></img>
             </td>
-            <td><?php echo $arbusto['ClassificazionePianteErbacee']?></td>
-            <td><?php echo $arbusto['ColorePianteErbacee']?></td> 
-            <td><?php echo $arbusto['Stagione_fioritura'] ?></td>
+            <td><?php echo $piantaErbacea['ClassificazionePianteErbacee']?></td>
+            <td><?php echo $piantaErbacea['ColorePianteErbacee']?></td> 
+            <td><?php echo $piantaErbacea['Stagione_fioritura'] ?></td>
          </tr>
          <?php
       } 
