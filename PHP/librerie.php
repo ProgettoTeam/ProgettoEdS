a40d6f73fe0cb186da6083f642f0db4d1bd502ec
teomirza02 <toader.mirza.studenti@isii.it>
2021-04-09 15:21:09 +0200

    Update librerie.php

--- PHP/librerie.php
+++ PHP/librerie.php
@@ -2,10 +2,10 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
     
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/29dd549171.js" crossorigin="anonymous"></script>
 
 
     <link rel="stylesheet" href="../../CSS/style.css">
