c884fabecd9eb50f3ce3a3bc3b3b5f3477810e9b
zuzanna1302 <74589556+zuzanna1302@users.noreply.github.com>
2021-04-10 16:31:47 +0200

    sistemazione classi della pagina

--- PHP/Aboutus.php
+++ PHP/Aboutus.php
@@ -28,8 +28,8 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h1 class="lg-text">About Us</h1>
-                    <p class="image-aboutus-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h1 class="lg-text h1-aboutus">About Us</h1>
+                    <p class="image-aboutus-para p-aboutus">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
             </div>
         </div>
@@ -39,12 +39,12 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    <h1 class="strong">Who we are and<br>what we do</h1>
-                    <p class="lead">This is the world's leading portal for<br>easy and quick </p>
+                    <h1 class="strong h1-aboutus" >Who we are and<br>what we do</h1>
+                    <p class="lead p-aboutus">This is the world's leading portal for<br>easy and quick </p>
                 </div>
                 <div class="col-md-6">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis egestas rhoncus. Donec facilisis fermentum sem, ac viverra ante luctus vel. Donec vel mauris quam.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis egestas rhoncus. Donec facilisis fermentum sem, ac viverra ante luctus vel. Donec vel mauris quam. Lorem ipsum dolor sit amet. Nulla convallis egestas rhoncus.</p>
+                    <p class="p-aboutus">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis egestas rhoncus. Donec facilisis fermentum sem, ac viverra ante luctus vel. Donec vel mauris quam.</p>
+                    <p class="p-aboutus">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis egestas rhoncus. Donec facilisis fermentum sem, ac viverra ante luctus vel. Donec vel mauris quam. Lorem ipsum dolor sit amet. Nulla convallis egestas rhoncus.</p>
                 </div>
             </div>
         </div>
@@ -52,8 +52,8 @@
     <div class="container team-sektion paddingTB60">
         <div class="row">
             <div class="site-heading text-center">
-                <h3>Our Team</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt <br> ut labore et dolore magna aliqua. Ut enim ad minim </p>
+                <h3 class="h3-aboutus">Our Team</h3>
+                <p class="p-aboutus">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt <br> ut labore et dolore magna aliqua. Ut enim ad minim </p>
                 <div class="border"></div>
             </div>
         </div>
@@ -62,14 +62,14 @@
                 <div class="team-img thumbnail">
                     <img src="../CSS/teo.jpg">
                     <div class="team-content">
-                        <h3>Toader Mirza</h3>
+                        <h3 class="h3-aboutus">Toader Mirza</h3>
                         <div class="border-team"></div>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis egestas rhoncus. Donec facilisis fermentum sem, ac viverra ante luctus vel. Donec vel mauris quam.</p>
                         <div class="social-icons">
-                            <a href="https://www.facebook.com/teomirza.mirza"><i id="social-fb" class="fa fa-facebook-square fa-3x social i-aboutus"></i></a>
-                            <a href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social i-aboutus"></i></a>
-                            <a href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social i-aboutus"></i></a>
-                            <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social i-aboutus"></i></a>
+                            <a class="a-aboutus" href="https://www.facebook.com/teomirza.mirza"><i id="social-fb" class="fa fa-facebook-square fa-3x social i-aboutus"></i></a>
+                            <a class="a-aboutus" href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social i-aboutus"></i></a>
+                            <a class="a-aboutus" href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social i-aboutus"></i></a>
+                            <a class="a-aboutus" href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social i-aboutus"></i></a>
                         </div>
                     </div>
                 </div>
@@ -78,14 +78,14 @@
                 <div class="team-img thumbnail">
                     <img src="../CSS/zuza.jpg">
                     <div class="team-content">
-                        <h3>Zuzanna Kacka</h3>
+                        <h3 class="h3-aboutus" >Zuzanna Kacka</h3>
                         <div class="border-team"></div>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis egestas rhoncus. Donec facilisis fermentum sem, ac viverra ante luctus vel. Donec vel mauris quam.</p>
                         <div class="social-icons">
-                            <a href="https://www.facebook.com/zuzanna.kacka"><i id="social-fb" class="fa fa-facebook-square fa-3x social i-aboutus"></i></a>
-                            <a href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social i-aboutus"></i></a>
-                            <a href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social i-aboutus"></i></a>
-                            <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social i-aboutus"></i></a>
+                            <a class="a-aboutus"  href="https://www.facebook.com/zuzanna.kacka"><i id="social-fb" class="fa fa-facebook-square fa-3x social i-aboutus"></i></a>
+                            <a class="a-aboutus" href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social i-aboutus"></i></a>
+                            <a class="a-aboutus" href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social i-aboutus"></i></a>
+                            <a class="a-aboutus" href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social i-aboutus"></i></a>
                         </div>
                     </div>
                 </div>
@@ -94,14 +94,14 @@
                 <div class="team-img thumbnail">
                     <img src="../CSS/tosca .jpg">
                     <div class="team-content">
-                        <h3>Tommaso Tosca</h3>
+                        <h3 class="h3-aboutus" >Tommaso Tosca</h3>
                         <div class="border-team"></div>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis egestas rhoncus. Donec facilisis fermentum sem, ac viverra ante luctus vel. Donec vel mauris quam.</p>
                         <div class="social-icons">
-                            <a href="https://it-it.facebook.com/tommaso.tosca"><i id="social-fb" class="fa fa-facebook-square fa-3x social i-aboutus"></i></a>
-                            <a href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social i-aboutus"></i></a>
-                            <a href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social i-aboutus"></i></a>
-                            <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social i-aboutus"></i></a>
+                            <a class="a-aboutus"  href="https://it-it.facebook.com/tommaso.tosca"><i id="social-fb" class="fa fa-facebook-square fa-3x social i-aboutus"></i></a>
+                            <a class="a-aboutus"  href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social i-aboutus"></i></a>
+                            <a class="a-aboutus" href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social i-aboutus"></i></a>
+                            <a class="a-aboutus" href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social i-aboutus"></i></a>
                         </div>
                     </div>
                 </div>
